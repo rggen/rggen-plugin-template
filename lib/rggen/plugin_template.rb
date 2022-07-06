d8d30cd45c63940073774d94b8c9f104de8c8d9d
Taichi Ishitani <taichi730@gmail.com>
2022-07-06 23:25:19 +0900

    update the plugin template due to rggen/rggen#108

--- lib/rggen/plugin_template.rb
+++ lib/rggen/plugin_template.rb
@@ -2,14 +2,9 @@
 
 require_relative 'plugin_template/version'
 
-module RgGen
-  module PluginTemplate
-    extend Core::Plugin
-
-    setup_plugin :'rggen-plugin-template' do |plugin|
-      plugin.files [
-        'plugin_template/bit_field/type/foo'
-      ]
-    end
-  end
+RgGen.setup_plugin :'rggen-plugin-template' do |plugin|
+  plugin.version RgGen::PluginTemplate::VERSION
+  plugin.files [
+    'plugin_template/bit_field/type/foo'
+  ]
 end
--- lib/rggen/plugin_template/setup.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require File.expand_path('../plugin_template', __dir__)
-
-RgGen.register_plugin RgGen::PluginTemplate do |builder|
-  builder.enable :bit_field, :type, [:foo]
-end
